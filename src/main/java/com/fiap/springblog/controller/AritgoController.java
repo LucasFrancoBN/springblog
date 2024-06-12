3ca5e9c1ab17acd70b86fd6582f90bb75a6673b9
scl-lnavarro <126281004+scl-lnavarro@users.noreply.github.com>
2024-06-12 16:57:44 -0300

    metade da aula III

---
+++ src/main/java/com/fiap/springblog/controller/AritgoController.java
@@ -0,0 +1,9 @@
+package com.fiap.springblog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/artigos")
+public class AritgoController {
+}
---
+++ src/main/java/com/fiap/springblog/model/Artigo.java
@@ -0,0 +1,22 @@
+package com.fiap.springblog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Document
+public class Artigo {
+    @Id
+    private String codigo;
+    private String titulo;
+    private LocalDateTime data;
+    private String texto;
+    private String url;
+    private Integer status;
+
+}
