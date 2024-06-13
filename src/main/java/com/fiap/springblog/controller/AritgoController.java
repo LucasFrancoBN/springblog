d6b1b4ed8e059a249d700dc592ff227ac907122a
scl-lnavarro <126281004+scl-lnavarro@users.noreply.github.com>
2024-06-13 07:58:53 -0300

    finalizada aula IV

--- src/main/java/com/fiap/springblog/controller/AritgoController.java
+++ src/main/java/com/fiap/springblog/controller/AritgoController.java
@@ -1,9 +1,28 @@
 package com.fiap.springblog.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fiap.springblog.model.Artigo;
+import com.fiap.springblog.service.ArtigoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/artigos")
 public class AritgoController {
+
+    @GetMapping
+    public List<Artigo> obterTodos() {
+        return null;
+    }
+
+    @GetMapping("/{codigo}")
+    public Artigo obterPorCodigo(@PathVariable String codigo) {
+        return null;
+    }
+
+    @PostMapping
+    public Artigo criar(@RequestBody Artigo artigo) {
+        return null;
+    }
 }
